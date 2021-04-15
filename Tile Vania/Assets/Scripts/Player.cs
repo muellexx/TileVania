8cb4844a3af28b585d1ad0a6ca7e1ab66cc69096
muellexx <alex.muellex@gmail.com>
2021-04-15 15:13:45 +0900

    Player Jump Added

--- Tile Vania/Assets/Scripts/Player.cs
+++ Tile Vania/Assets/Scripts/Player.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Config
     [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
 
+    // State
+    bool isAlive = true;
+
+    // Cached References
     Rigidbody2D myRigidBody;
     SpriteRenderer myRenderer;
+    Animator myAnimator;
+    Collider2D myCollider;
+
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myRenderer = GetComponent<SpriteRenderer>();
+        myAnimator = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        Jump();
         FlipSprite();
     }
 
@@ -28,6 +40,20 @@ public class Player : MonoBehaviour
 
         Vector2 playerVelocity = new Vector2(deltaX, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+        
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void Jump()
+    {
+        if (!myCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) return;
+        if (Input.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
+        
     }
 
     private void FlipSprite()
